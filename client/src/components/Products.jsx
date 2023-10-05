6f85bbc1e7d5730ce8ab9d0a4b94e57aa8298c93
Medusa Collins <enesfurkanceylan@hotmail.com>
2023-10-06 00:14:33 +0300

    products sizing changes

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -5,6 +5,11 @@ const Products = () => {
   return (
     <div className='flex gap-5'>
         <ProductTemplate name="Skipper Collar Rayon Blouse" star="5" review="123" price="100,00" img="https://dolap.dsmcdn.com/dlp_231003_1/product/org/kadin/bluz/s-36-h-m_1244524095.jpg"/>
+        <ProductTemplate name="Skipper Collar Rayon Blouse" star="5" review="123" price="100,00" img="https://dolap.dsmcdn.com/dlp_231003_1/product/org/kadin/bluz/s-36-h-m_1244524095.jpg"/>
+        <ProductTemplate name="Skipper Collar Rayon Blouse" star="5" review="123" price="100,00" img="https://dolap.dsmcdn.com/dlp_231003_1/product/org/kadin/bluz/s-36-h-m_1244524095.jpg"/>
+        <ProductTemplate name="Skipper Collar Rayon Blouse" star="5" review="123" price="100,00" img="https://dolap.dsmcdn.com/dlp_231003_1/product/org/kadin/bluz/s-36-h-m_1244524095.jpg"/>
+        <ProductTemplate name="Skipper Collar Rayon Blouse" star="5" review="123" price="100,00" img="https://dolap.dsmcdn.com/dlp_231003_1/product/org/kadin/bluz/s-36-h-m_1244524095.jpg"/>
+        <ProductTemplate name="Skipper Collar Rayon Blouse" star="5" review="123" price="100,00" img="https://dolap.dsmcdn.com/dlp_231003_1/product/org/kadin/bluz/s-36-h-m_1244524095.jpg"/>
     </div>
   )
 }
--- client/src/pages/Woman.jsx
+++ client/src/pages/Woman.jsx
@@ -4,10 +4,10 @@ import Promo from '../components/Promo.jsx';
 import Products from '../components/Products.jsx';
 const Woman = () => {
   return (
-    <div className='p-5 grid gap-4'>
+    <div className='p-5 mx-24 gap-4'>
       <h1 className='text-xl font-semibold'>Women <span className='text-gray-300'>(123)</span></h1>
-      <Filter/>
-      <Promo/>
+      <div className='my-5'><Filter/></div>
+      <div className='my-5'><Promo/></div>
       <Products/>
     </div>
   )
